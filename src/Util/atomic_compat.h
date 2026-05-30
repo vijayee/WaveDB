4520022988fb721e58ad5ee3fe2826aa6b69f09c
Victor Morrow <victor.j.morrow@gmail.com>
2026-05-29 20:49:58 -0400

    feat: add liboffs-compatible atomics and platform thread layer

--- src/Util/atomic_compat.h
+++ src/Util/atomic_compat.h
@@ -30,4 +30,25 @@
   #define ATOMIC_VAR_INIT_COMPAT(v) ATOMIC_VAR_INIT(v)
 #endif
 
+/* Compatibility with liboffs actor code (uses method-style atomics) */
+#ifdef __cplusplus
+  #define ATOMIC(T) std::atomic<T>
+  #define ATOMIC_STORE(ptr, val) (ptr)->store(val)
+  #define ATOMIC_LOAD(ptr) (ptr)->load()
+  #define ATOMIC_EXCHANGE(ptr, val) (ptr)->exchange(val)
+  #define ATOMIC_FETCH_ADD(ptr, val) (ptr)->fetch_add(val)
+  #define ATOMIC_FETCH_OR(ptr, val) (ptr)->fetch_or(val)
+  #define ATOMIC_FETCH_AND(ptr, val) (ptr)->fetch_and(val)
+  #define ATOMIC_CAS_STRONG(ptr, expected, desired) (ptr)->compare_exchange_strong(expected, desired)
+#else
+  #define ATOMIC(T) _Atomic(T)
+  #define ATOMIC_STORE(ptr, val) atomic_store(ptr, val)
+  #define ATOMIC_LOAD(ptr) atomic_load(ptr)
+  #define ATOMIC_EXCHANGE(ptr, val) atomic_exchange(ptr, val)
+  #define ATOMIC_FETCH_ADD(ptr, val) atomic_fetch_add(ptr, val)
+  #define ATOMIC_FETCH_OR(ptr, val) atomic_fetch_or(ptr, val)
+  #define ATOMIC_FETCH_AND(ptr, val) atomic_fetch_and(ptr, val)
+  #define ATOMIC_CAS_STRONG(ptr, expected, desired) atomic_compare_exchange_strong(ptr, expected, desired)
+#endif
+
 #endif // WAVEDB_ATOMIC_COMPAT_H
